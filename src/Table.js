b359de1792093313d047d4be9f9ad2f2585fa859
JoshuaPerri <joshuaperri1@gmail.com>
2024-11-18 02:18:45 -0500

    Create basic board layout

--- src/Table.js
+++ src/Table.js
@@ -1,10 +1,30 @@
 import './Table.css';
 
+function Cell(props) {
+  const click = (a, b) => {
+    let button = b.target;
+    if (button.classList.contains("black")) {
+      button.classList.remove("black")
+    } else {
+      button.classList.add("black")
+    }
+  }
+
+  return (
+    <button className="Cell" onClick={(event) => click(props.num, event)}></button>
+  )
+}
+
 function Table() {
-  const list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  const list = []
+  for (let i = 0; i < 100; i++) {
+    list.push(i)
+  }
   return (
     <div className="Table">
-      {list.map((i) => <div>{i}</div>)}
+      {list.map(i => 
+        <Cell key={i} num={i}></Cell>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Table from './Table.js';
+import './Table.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Table />
+    <div id="container">
+      <Table />
+    </div>
   </React.StrictMode>
 );
 

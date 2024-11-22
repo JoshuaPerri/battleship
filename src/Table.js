cb543c75c0202de5a6fffac75302abb092afbf4e
JoshuaPerri <joshuaperri1@gmail.com>
2024-11-21 23:37:46 -0500

    Test UseState

--- src/Table.js
+++ src/Table.js
@@ -1,20 +1,41 @@
 import './Table.css';
+import { useState } from 'react';
+
+let max_shots = 5
+let shots_remaining = max_shots
 
 function toggleClass(element, className) {
-  element.classList.contains(className) ? element.classList.remove(className) : element.classList.add(className)
+  if (element.classList.contains(className)) {
+    element.classList.remove(className)
+    return true
+  } else {
+    element.classList.add(className)
+    return false
+  }
 }
 
 function Cell(props) {
-  const click = (a, b) => {
-    let button = b.currentTarget
+  const click = (e) => {
+    let button = e.currentTarget
     // Search children of button to get token
     let token = Array.from(button.children).filter((child) => child.classList.contains("Token"))[0];
-    toggleClass(token, "hidden")
+
+    if (token.classList.contains("hidden")) {
+      if (shots_remaining > 0) {
+        shots_remaining--
+        token.classList.remove("hidden")
+      }
+    } else {
+      if (shots_remaining < max_shots) {
+        shots_remaining++
+        token.classList.add("hidden")
+      }
+    }
   }
 
   return (
-    <button className="Cell" onClick={(event) => click(props.num, event)}>
-      <div className='Token hidden'></div>
+    <button className="Cell" onClick={(event) => click(event)}>
+      <div className='Token staged hidden' col={props.col} row={props.row}></div>
     </button>
   )
 }
@@ -27,7 +48,7 @@ function Table() {
   return (
     <div className="Table">
       {list.map(i => 
-        <Cell key={i} num={i}></Cell>
+        <Cell key={i} row={(i - i % 10) / 10} col={i % 10}></Cell>
       )}
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,13 +4,76 @@ import './index.css';
 import Table from './Table.js';
 import './Table.css';
 import reportWebVitals from './reportWebVitals';
+import { useState } from 'react';
+
+let board = []
+for (let i = 0; i < 10; i++) {
+  let row = []
+  for (let i = 0; i < 10; i++) {
+    row.push(i % 2)
+  }
+  board.push(row)
+}
+console.log(board)
+
+const click = (e) => {
+  let tokens = document.querySelectorAll(".Token.staged:not(.hidden)")
+
+  Array.from(tokens).forEach((token) => {
+    let row = parseInt(token.getAttribute("row"))
+    let col = parseInt(token.getAttribute("col"))
+    if (board[row][col] == 1) {
+      token.classList.remove("staged")
+      token.classList.add("hit")
+    } else {
+      token.classList.remove("staged")
+      token.classList.add("miss")
+    }
+  })
+}
+
+
+function TestButton(props) {
+  const click = (e, props) => {
+    props.setState({
+      value: props.state.value + 1
+    })
+  }
+
+  return (
+    <button className="TestButton" onClick={(e) => click(e, props)}>
+      {props.state.value}
+    </button>
+  )
+}
+
+function Test() {
+  const [state, setState] = useState({
+    value: 0
+  })
+  // const click = (e) => {
+  //   setState({
+  //     value: state.value + 1
+  //   })
+  // }
+
+  return (
+    <div className='Test'>
+      {/* <Test2 state={state} setState={setState}/> */}
+      {/* <button onClick={(e) => click(e)}>{state.value}</button> */}
+      <TestButton state={state} setState={setState}/>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div id="container">
+    {/* <div id="container">
       <Table />
     </div>
+    <button onClick={(event) => click(event)}>Fire</button> */}
+    <Test/>
   </React.StrictMode>
 );
 

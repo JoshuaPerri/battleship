3b86cbf54acfc444092ead66bdcb737d3f96cea1
JoshuaPerri <joshuaperri1@gmail.com>
2024-11-25 20:37:24 -0500

    Rewrite to use useState

--- src/game.js
+++ src/game.js
@@ -2,7 +2,6 @@ import './game.css';
 import { useState } from 'react';
 
 let max_shots = 5
-let shots_remaining = max_shots
 
 function toggleClass(element, className) {
   if (element.classList.contains(className)) {
@@ -21,14 +20,14 @@ function Cell(props) {
     let token = Array.from(button.children).filter((child) => child.classList.contains("Token"))[0];
 
     if (token.classList.contains("hidden")) {
-      if (shots_remaining > 0) {
-        shots_remaining--
+      if (props.gameState.shotsRemaining > 0) {
         token.classList.remove("hidden")
+        props.onStage();
       }
     } else {
-      if (shots_remaining < max_shots) {
-        shots_remaining++
+      if (props.gameState.shotsRemaining < max_shots) {
         token.classList.add("hidden")
+        props.onUnstage();
       }
     }
   }
@@ -40,52 +39,65 @@ function Cell(props) {
   )
 }
 
-function Table() {
+function Table({gameState, onStage, onUnstage}) {
   const list = []
   for (let i = 0; i < 100; i++) {
     list.push(i)
   }
+
   return (
     <div className="Table">
       {list.map(i => 
-        <Cell key={i} row={(i - i % 10) / 10} col={i % 10}></Cell>
+        <Cell key={i} row={(i - i % 10) / 10} col={i % 10} gameState={gameState} onStage={onStage} onUnstage={onUnstage}></Cell>
       )}
     </div>
   );
 }
 
-let board = []
-for (let i = 0; i < 10; i++) {
-  let row = []
-  for (let i = 0; i < 10; i++) {
-    row.push(i % 2)
-  }
-  board.push(row)
-}
-console.log(board)
-
-const click = (e) => {
-  let tokens = document.querySelectorAll(".Token.staged:not(.hidden)")
-
-  Array.from(tokens).forEach((token) => {
-    let row = parseInt(token.getAttribute("row"))
-    let col = parseInt(token.getAttribute("col"))
-    if (board[row][col] == 1) {
-      token.classList.remove("staged")
-      token.classList.add("hit")
-    } else {
-      token.classList.remove("staged")
-      token.classList.add("miss")
-    }
-  })
-}
 
 
 function Game() {
+
+  const board = []
+  for (let i = 0; i < 10; i++) {
+    let row = []
+    for (let i = 0; i < 10; i++) {
+      row.push(i % 2)
+    }
+    board.push(row)
+  }
+  console.log(board)
+
+  const [gameState, setGameState] = useState({
+    shotsRemaining: 5,
+    board: board
+  })
+
+  const click = (e) => {
+    let tokens = document.querySelectorAll(".Token.staged:not(.hidden)")
+
+    Array.from(tokens).forEach((token) => {
+      let row = parseInt(token.getAttribute("row"))
+      let col = parseInt(token.getAttribute("col"))
+      if (board[row][col] == 1) {
+        token.classList.remove("staged")
+        token.classList.add("hit")
+      } else {
+        token.classList.remove("staged")
+        token.classList.add("miss")
+      }
+    })
+    setGameState({...gameState, shotsRemaining: 5})
+  }
+
   return (
     <div className='Game'>
       <div id="container">
-        <Table />
+        <Table 
+          gameState={gameState} 
+          onStage={() =>   setGameState({...gameState, shotsRemaining: gameState.shotsRemaining - 1})} 
+          onUnstage={() => setGameState({...gameState, shotsRemaining: gameState.shotsRemaining + 1})} 
+        />
       </div>
       <button onClick={(event) => click(event)}>Fire</button>
     </div>
